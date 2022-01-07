3aa7ae7370a180eeeea83304135d68707910fb85
theo.kim <theo.kim@stagefive.com>
2022-01-07 19:00:05 +0900

    temp

---
+++ src/pages/404.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from 'next';
+
+const Custom404: NextPage = () => (
+  <>
+    <h1>404 - Page Not Found</h1>
+    <p>404 에러 페이지 입니다.</p>
+  </>
+);
+
+export default Custom404;
---
+++ src/pages/500.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from 'next';
+
+const Custom500: NextPage = () => (
+  <>
+    <h1>500 - Page Not Found</h1>
+    <p>500 에러 페이지 입니다.</p>
+  </>
+);
+
+export default Custom500;
